20e352dcf2b0248bc352660f7a2c46ccaeb780c0
davis <darapac@ulasalle.edu.pe>
2026-06-30 12:49:08 -0500

    quitanto url local en api.js

--- frontend-vue/src/services/api.js
+++ frontend-vue/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import i18n from '@/i18n'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api' || 'https://sgcp-proyecto.vercel.app/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://sgcp-proyecto.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
   }
